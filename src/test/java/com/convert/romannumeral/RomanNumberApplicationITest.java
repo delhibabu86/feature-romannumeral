6b0e2d2ef516296077048ad0b24449945ceb2d98
dvengambhanumoorthy <delhibabu86@gmail.com>
2021-10-09 13:20:11 -0600

    code fix based on sonar qube scan

--- src/test/java/com/convert/romannumeral/RomanNumberApplicationITest.java
+++ src/test/java/com/convert/romannumeral/RomanNumberApplicationITest.java
@@ -27,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest(classes = RomanNumberApplication.class)
 @AutoConfigureMockMvc
-class IRomanNumberApplicationTest {
+class RomanNumberApplicationITest {
 
     @Autowired
     MockMvc mockMvc;
--- src/test/java/com/convert/romannumeral/RomanNumberApplicationTest.java
+++ src/test/java/com/convert/romannumeral/RomanNumberApplicationTest.java
@@ -1,15 +1,41 @@
 package com.convert.romannumeral;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static org.mockito.Mockito.mockStatic;
 
 /**
  * @author dvengambhanumoorthy
  */
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class RomanNumberApplicationTest {
+    final String[] args = {"TEST"};
+    @InjectMocks
+    RomanNumberApplication romanNumberApplication;
+    @Mock
+    ConfigurableApplicationContext configurableApplicationContext;
+    @Mock
+    SpringApplication springApplication;
+
     @Test
-    void contextLoads() {
+    void appMain() {
+        try (MockedStatic<SpringApplication> mockedStatic = mockStatic(SpringApplication.class,
+                "run")) {
+            mockedStatic
+                    .when(() -> SpringApplication.run(ArgumentMatchers.any(Class.class),
+                            ArgumentMatchers.eq(args))).thenReturn(this.configurableApplicationContext);
+            RomanNumberApplication.main(args);
+            Assertions.assertNotNull(this.configurableApplicationContext);
+        }
     }
 }
 
